b181d7f34c2acc3ca04fa4a51046acc0139456af
Elvis Guanoluisa <elviserock@gmail.com>
2023-08-14 12:38:28 -0500

    Modulo confirmar cuenta

--- FrontEnd/src/paginas/Register.jsx
+++ FrontEnd/src/paginas/Register.jsx
@@ -25,7 +25,10 @@ export const Register = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         try {
-            const url = "http://localhost:3000/api/registro"
+            // const url = "http://localhost:3000/api/registro"
+            console.log('hola1')
+            const url = `${import.meta.env.VITE_BACKEND_URL}/registro`
+            console.log('hola2')
             const respuesta = await axios.post(url, form)
             setMensaje({ respuesta: respuesta.data.msg, tipo: true })
             setform({})
